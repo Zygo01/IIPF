87d3947e46da39c9fe048d7cadf4f19c50f0d1f9
Zygo01 <kjagla0@gmail.com>
2022-03-29 12:29:11 +0200

    final
    goto z komentarzami

--- 12/main.m
+++ 12/main.m
@@ -1,4 +1,4 @@
-clear("M","Mt","Mr","Mg","Mb","My");
+#clear("M1","M2","M3","M4","M5");
 Mt = csvread("Data/transparent.csv");
 #{
 Mr = csvread("Data/red.csv");
@@ -6,11 +6,63 @@ Mg = csvread("Data/green.csv");
 Mb = csvread("Data/blue.csv");
 My = csvread("Data/yellow.csv");
 #}
-[p,s] = polyfit(Mt(1:end,2),Mt(1:end,1), 0);
 
-plot(Mt(1:end,2),Mt(1:end,1),".r","markersize", 15)
-     grid minor on
-     xlabel("Thickness");
-     ylabel("ABS");
-print ("1.pdf")
+
+[pT,s] = polyfit(Mt(1:end,2),Mt(1:end,1), 1);
+PolyT = polyval(pT, linspace(Mt(1,1), Mt(end,1), 50));
+#{
+[pT,s] = polyfit(Mr(1:end,2),Mr(1:end,1), 1);
+PolyT = polyval(pT, linspace(Mr(1,1), Mr(end,1), 50));
+[pT,s] = polyfit(Mg(1:end,2),Mg(1:end,1), 1);
+PolyT = polyval(pT, linspace(Mg(1,1), Mg(end,1), 50));
+[pT,s] = polyfit(Mb(1:end,2),Mb(1:end,1), 1);
+PolyT = polyval(pT, linspace(Mb(1,1), Mb(end,1), 50));
+[pT,s] = polyfit(My(1:end,2),My(1:end,1), 1);
+PolyT = polyval(pT, linspace(My(1,1), My(end,1), 50));
+#}
+
+figure(1);
+plot(Mt(1:end,2),Mt(1:end,1),".r","markersize",15,
+    linspace(Mt(1,1),Mt(end,1),50),PolyT,"linewidth",2);
+grid minor on
+xlabel("Thickness");
+ylabel("ABS");
+print ("Plots/Transparent.pdf")
 close()
+#{
+figure(1);
+plot(a(1:end,2),a(1:end,1),".r","markersize",15,
+    linspace(a(1,1),a(end,1),50),PolyT,"linewidth",2)
+grid minor on
+xlabel("Thickness");
+ylabel("ABS");
+print (strcat("Plots/", int2str(i),".pdf"))
+close()
+
+figure(1);
+plot(a(1:end,2),a(1:end,1),".r","markersize",15,
+    linspace(a(1,1),a(end,1),50),PolyT,"linewidth",2)
+grid minor on
+xlabel("Thickness");
+ylabel("ABS");
+print (strcat("Plots/", int2str(i),".pdf"))
+close()
+
+figure(1);
+plot(a(1:end,2),a(1:end,1),".r","markersize",15,
+    linspace(a(1,1),a(end,1),50),PolyT,"linewidth",2)
+grid minor on
+xlabel("Thickness");
+ylabel("ABS");
+print (strcat("Plots/", int2str(i),".pdf"))
+close()
+
+figure(1);
+plot(a(1:end,2),a(1:end,1),".r","markersize",15,
+    linspace(a(1,1),a(end,1),50),PolyT,"linewidth",2)
+grid minor on
+xlabel("Thickness");
+ylabel("ABS");
+print (strcat("Plots/", int2str(i),".pdf"))
+close()
+#}
