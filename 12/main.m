f5249b8c062c4df0948db9f3425dca049532d228
Zygo01 <kjagla0@gmail.com>
2022-03-29 17:23:21 +0200

    Final!!
    added coefficient
    hopefully the end of 12

--- 12/main.m
+++ 12/main.m
@@ -4,21 +4,37 @@ Mg = csvread("Data/green.csv");
 Mb = csvread("Data/blue.csv");
 My = csvread("Data/yellow.csv");
 
+Y1 = linspace(Mt(1,2),Mt(end,2),50);
 Y2 = linspace(Mb(1,2),Mb(end,2),50);
+#{
+Output linear coefficient A of specific color
+next is delta A of specific color
+last is Alfa of specific color
+#}
+#-------------------------------------------------------------
 
-pT= polyfit(Mt(1:end,2),Mt(1:end,1), 1);
-PolyT = polyval(pT, linspace(Mt(1,2), Mt(end,2), 50));
+[pT, s]= polyfit(Mt(1:end,2),Mt(1:end,1), 1);
+PolyT = polyval(pT, Y1, 50);
+At = pT
+DeltaAt = sqrt (diag (s.C)/s.df)*s.normr
+AlfaT = At(1)/0.434294
 figure(1);
 plot(Mt(1:end,2),Mt(1:end,1),".r","markersize",15,
-    linspace(Mt(1,2),Mt(end,2),50),PolyT,"linewidth",2);
+    Y1 ,PolyT,"linewidth",2);
 grid minor on
 xlabel("Thickness");
 ylabel("ABS");
 print ("Plots/Transparent.pdf")
 close()
 
-pT= polyfit(Mr(1:end,2),Mr(1:end,1), 1);
+#-------------------------------------------------------------
+
+[pT, s]= polyfit(Mr(1:end,2),Mr(1:end,1), 1);
 PolyT = polyval(pT, Y2);
+Ar = pT
+DeltaAr = sqrt (diag (s.C)/s.df)*s.normr
+AlfaR = Ar(1)/0.434294
+
 figure(2);
 plot(Mr(1:end,2),Mr(1:end,1),".r","markersize",15,
     Y2,PolyT,"linewidth",2);
@@ -28,8 +44,14 @@ ylabel("ABS");
 print (strcat("Plots/Red.pdf"))
 close()
 
+#-------------------------------------------------------------
+
 [pT,s] = polyfit(Mg(1:end,2),Mg(1:end,1), 1);
 PolyT = polyval(pT, Y2);
+Ag = pT
+DeltaAg = sqrt (diag (s.C)/s.df)*s.normr
+AlfaG = Ag(1)/0.434294
+
 figure(3);
 plot(Mg(1:end,2),Mg(1:end,1),".r","markersize",15,
     Y2,PolyT,"linewidth",2);
@@ -39,8 +61,14 @@ ylabel("ABS");
 print (strcat("Plots/Green.pdf"))
 close()
 
+#-------------------------------------------------------------
+
 [pT,s] = polyfit(Mb(1:end,2),Mb(1:end,1), 1);
 PolyT = polyval(pT, Y2, 50);
+Ab = pT
+DeltaAb = sqrt (diag (s.C)/s.df)*s.normr
+AlfaB = Ab(1)/0.434294
+
 figure(4);
 plot(Mb(1:end,2),Mb(1:end,1),".r","markersize",15,
     Y2,PolyT,"linewidth",2);
@@ -50,8 +78,14 @@ ylabel("ABS");
 print (strcat("Plots/Blue.pdf"))
 close()
 
+#-------------------------------------------------------------
+
 [pT,s] = polyfit(My(1:end,2),My(1:end,1), 1);
 PolyT = polyval(pT, Y2);
+Ay = pT
+DeltaAy = sqrt (diag (s.C)/s.df)*s.normr
+AlfaY = Ay(1)/0.434294
+
 figure(5);
 plot(My(1:end,2),My(1:end,1),".r","markersize",15,
     Y2,PolyT,"linewidth",2);
