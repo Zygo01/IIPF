358d72108f023b94d361b4417f3b9d21db2a6994
Zygo01 <kjagla0@gmail.com>
2022-04-03 23:35:15 +0200

    testing
    testing some plots

--- 14/main.m
+++ 14/main.m
@@ -4,48 +4,45 @@ Ub=M(1:end,1);Ib=M(1:end,2);Ug=M(1:21,3);Ig=M(1:21,4);Uy=M(1:14,5);Iy=M(1:14,6);
 N = csvread("data2.csv");
 
 % Najlepsze dopasowanie f. liniowej do środkowej części sygmoidu
-function [c] = fitness (a,b)
-    for i = 1:length(a)/2
-    [p, s] = polyfit(a(0+i:(length(a)+1)-i), b(0+i:(length(a)+1)-i), 1);
-    f(i,1:2) = p;
-    y(i,1:length(a)) = (polyval(p, a));
-    e(i,1) = sum(sqrt (diag (s.C)/s.df)*s.normr);
-  endfor
-  [k iw] = min(e);
-  c = f(iw,:);
-endfunction
+pB = polyfit(Ub(3:length(Ub)-3), Ib(3:length(Ib)-3), 1);
+polyB = (polyval(pB, Ub));
 
+pG = polyfit(Ug(9:length(Ug)-5), Ig(9:length(Ig)-5), 1);
+polyG = (polyval(pG, Ug));
+
+pY = polyfit(Uy(6:length(Uy)-6), Iy(6:length(Iy)-6), 1);
+polyY = (polyval(pY, Uy));
 %-------------------------------------------------------------------------
 % Krzywa dyspersji
-#{
-[dysP, s] = polyfit(N(1:end,1),N(1:end,2),2);
+
+[dysP, s] = polyfit(N(1:end,1),N(1:end,2),1);
 dysp = polyval(dysP, linspace(N(1,1), N(end,1), 50));
 figure(4);
 plot(N(1:end,1),N(1:end,2),".k","markersize", 15,
-     N(1:end,1),N(1:end,2),"linewidth",2,
      linspace(N(1,1), N(end,1), 50),dysp,"linewidth",2)
      grid minor on
 set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
 print dispersion2.pdf
+close();
 
 % Krzywa dyspersji log
 
 
 figure(8);
-y = loglog(N(1:end,2),N(1:end,1));
-[dysPLOG, s] = polyfit(N(1:end,1),y,2);
-dyspLOG = polyval(dysPLOG, linspace(N(1,1), N(end,1), 50));
-plot(linspace(N(1,1), N(end,1), 50),dyspLOG,"linewidth",2);
-axis("tight");
+semilogy(N(1:end,2),N(1:end,1),".k","markersize", 15);
+grid minor on
 print dispersionLOG.pdf
-#}
+close();
 
 %-------------------------------------------------------------------------
 % Napięcia Hamowania
 
-Uhb = roots(fitness(Ub, Ib));
-Uhg = roots(fitness(Ug, Ig));
-Uhy = roots(fitness(Uy, Iy));
+#Uhb = roots(pB);
+#Uhg = roots(pG);
+#Uhy = roots(pY);
+Uhb = -0.865;
+Uhg = -1.304;
+Uhy = -0.497;
 Uh = [abs(Uhb), abs(Uhg), abs(Uhy)];
 
 %-------------------------------------------------------------------------
@@ -62,18 +59,18 @@ h_poly = polyval(Uhf, linspace(fb, fy, 50));
 %-------------------------------------------------------------------------
 % Wyznaczanie stałej Plancka i błędów pomiarowych
 
-h = Uhf(1,1)* physical_constant("elementary charge");
+h = Uhf(1,1)* physical_constant("elementary charge")
 delta = sqrt (diag (sh.C)/sh.df)*sh.normr;
 deltaA = delta(1,1);
 
 %-------------------------------------------------------------------------
 % Wykresy
-#{
+
 figure(1);
 plot(Ub,Ib,".k","markersize", 15,
-     Ub,Ib,"linewidth",2,
      Uhb,0,".r","markersize",20,
-     Ub, polyval(fitness(Ub, Ib),Ub),"linewidth",2)
+     Ub, polyB
+     ,"linewidth",2)
      grid minor on
 set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
 print ("Blue.pdf");
@@ -81,9 +78,8 @@ close();
 
 figure(2);
 plot(Ug,Ig,".k","markersize", 15,
-     Ug,Ig,"linewidth",2,
      Uhg,0,".r","markersize",20,
-     Ug, polyval(fitness(Ug, Ig),Ug),"linewidth",2)
+     Ug, polyG,"linewidth",2)
      grid minor on
 set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
 print ("Green.pdf");
@@ -91,9 +87,8 @@ close();
 
 figure(3);
 plot(Uy,Iy,".k","markersize", 15,
-     Uy,Iy,"linewidth",2,
      Uhy,0,".r","markersize",20,
-     Uy, polyval(fitness(Uy, Iy),Uy),"linewidth",2)
+     Uy, polyY,"linewidth",2)
      grid minor on
 set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
 print ("Yellow.pdf");
@@ -101,16 +96,17 @@ close();
 
 figure(4);
 plot(N(1:end,1),N(1:end,2),".k","markersize", 15,
-     N(1:end,1),N(1:end,2),"linewidth",2,
      linspace(N(1,1), N(end,1), 50),dysp,"linewidth",2)
      grid minor on
 set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
 print dispersion.pdf
+close();
 
 figure(5);
 plot(f,Uh,".k","markersize", 15,
-     linspace(fb, fy, 50), h_poly,"linewidth",2)
-     grid minor on
-set (gca, "xaxislocation","origin", "yaxislocation", "origin", "box","off")
+     linspace(fb, fy, 50), h_poly,"linewidth",2);
+ylim([0, 1]);
+grid minor on
 print Final.pdf
-#}
+close();
+
